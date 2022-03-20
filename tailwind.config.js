530029101c0df14d3661dd25c56723f1693220db
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-03-20 21:36:39 +0100

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "hsl(209, 23%, 22%)",
+        "v-dark-blue": "hsl(207, 26%, 17%)",
+        "v-dark-blue-dark": "hsl(207, 26%, 17%)",
+        "v-dark-blue-light": "hsl(200, 15%, 8%)",
+        "dark-gray": "hsl(0, 0%, 52%)",
+        "v-dark-gray": "hsl(0, 0%, 98%)",
+      },
+      fontFamily: {
+        main: ["Nunito Sans"],
+      },
+    },
+  },
+  plugins: [],
+};
